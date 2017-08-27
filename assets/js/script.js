4335c8c08e4190a68431f64838a2b05dd12cf163
zorenko <zorenkoa@gmail.com>
2017-08-28 01:16:29 +0300

    updates
    - added text to jt project
    - new primary color
    - mobile version fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 function closeFunct() {
    document.querySelector(".alert").style.display='none';
   }
+
+
+function toggleDiv(element){
+
+  var el = document.getElementById(element);
+  if(el.style.display != 'block')
+  {
+    el.style.display = 'block';
+  }
+  else
+  {
+    el.style.display = '';
+  }
+}
